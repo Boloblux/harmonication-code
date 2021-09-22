2a46bdd40b459565f53a4344c028351d7070ea62
Nicolas de Garrigues <ndegarrigues@gmail.com>
2021-09-22 10:45:16 +0200

    :sparkles: ADD cypress

--- src/components/Sacha.vue
+++ src/components/Sacha.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container ">
-    <picture style="width:100%; display:flex; justify-content:center"><img  style="margin 0 auto; width:10%; " src="https://d1b50349hl2oei.cloudfront.net/img/logo/snc-mini.png" alt=""></picture>
+    <picture style="width:100%; display:flex; justify-content:center"><img  style="margin: 0 auto; width:10%; " src="https://d1b50349hl2oei.cloudfront.net/img/logo/snc-mini.png" alt=""></picture>
     <h2 style="text-align:center">Sacha Galeuzzi</h2>
     <h3 style="text-align:center">Alternant - developpeur web chez Sports N'Connect</h3>
     
