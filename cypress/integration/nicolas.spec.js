bbed90b05f6d9c709343b9c35aed2651fc398271
Nicolas de Garrigues <ndegarrigues@gmail.com>
2021-09-22 11:14:12 +0200

    :memo: RENAME files

--- cypress/integration/nicolas.spec.js
+++ cypress/integration/nicolas.spec.js
@@ -1,5 +1,5 @@
 describe('example to-do app', () => {
-    it('should visit a site', () => {
+    it('click on all buttons', () => {
         cy.visit('localhost:8080')
         cy.get('a').click({multiple: true})
         cy.get('a').should("have.attr", "href", "javascript:void(0)");
